36289a773f0e8d51fae4a51b90d18f5a275fcf36
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-08-07 20:59:07 +0100

    Control flow with Js

---
+++ index.js
@@ -0,0 +1,10 @@
+let crush = prompt('What is your name?');
+
+
+document.querySelector('button').addEventListener('click', function  () {
+    if (crush === 'Damilola') {
+        alert('I love you Damilola')
+    } else {
+        alert('Life is tough')
+    }
+    });

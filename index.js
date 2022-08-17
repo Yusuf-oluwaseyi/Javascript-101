592c5afbdc2fc4e77d0fa89e64e22bf881367ed1
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-08-17 12:14:52 +0100

    Javascript lessons

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-let crush = prompt('What is your name?');
+// let crush = prompt('What is your name?');
 
 
-document.querySelector('button').addEventListener('click', function  () {
-    if (crush === 'Damilola') {
-        alert('I love you Damilola')
-    } else {
-        alert('Life is tough')
-    }
-    });
+// document.querySelector('button').addEventListener('click', function  () {
+//     if (crush === 'Damilola') {
+//         alert('I love you Damilola')
+//     } else {
+//         alert('Life is tough')
+//     }
+//     });
+
+
+// using switch case for a prediction of eightball
+// var userQuestion = '';
+
+// var randomNumber = Math.floor(Math.random()*8);
+
+// var eightBall = randomNumber;
+
+// switch (eightBall) {
+//   case 1:
+//   console.log('It is certain');
+//   break;
+//   case 2:
+//   console.log('It is decidedly so');
+//   break;
+//   case 3:
+//   console.log('Reply hazy try again');
+//   break;
+//   case 4:
+//   console.log('Cannot predict now');
+//   break;
+//   case 5:
+//   console.log('Do not count on it');
+//   break;
+//   case 6:
+//   console.log('My sources say no');
+//   break;
+//   case 7:
+//   console.log('Outlook not so good');
+//   break;
+//   case 8:
+//   console.log('Signs point to yes');
+//   break;
+//   default:
+//   console.log('Come again');
+//   }
+
+// let today = new Date();
+
+// console.log(today);
+
+// let name = null;
+
+// console.log(typeOf name);
+

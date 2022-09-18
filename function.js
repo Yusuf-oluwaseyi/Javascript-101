c1e60562018c210f49ea54d1cfa27957605c0d04
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-09-18 05:42:35 +0100

    Adding functions

--- function.js
+++ function.js
@@ -12,3 +12,29 @@ const square = function (x) {
 };
 // console.log(square(5));
 // Creating an anonymous function and in this case called Function Expression.
+
+// IMMEDIATE IVOKABLE FUNCTIONS or IIFEs
+(function(){
+    // console.log("IIFEs Ran..");
+})();
+// You can also pass parameters into them
+(function(name){
+    // console.log('Hello ' + name)
+})('Toba');
+
+// Property method, adding functions in objects
+const todo = {
+    add: function(){
+        console.log('Add todo');
+    },
+    Edit: function(id){
+        console.log(`Edit todo ${id}`)
+    }
+}
+todo.delete = function(){
+    console.log('delete todo..');
+}
+
+todo.add();
+todo.Edit(22);
+todo.delete();

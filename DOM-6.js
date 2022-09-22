0867f9c7bd433f9fbd587512cf98c403e529fca8
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-09-22 13:12:06 +0100

    Dom-6

---
+++ DOM-6.js
@@ -0,0 +1,20 @@
+let val;
+
+// The documents can be used to grab different elements from the web page.
+
+// val = document.html;
+// val = document.links;
+// val = document.scripts;
+
+//  Using foreach to get scripts, which means turning it to an array before using the forEachach method.
+// let scripts = document.scripts;
+
+// let scriptArr = Array.from(scripts);
+
+// scriptArr.forEach(function(script) {
+
+//     console.log(script.getAttribute('src'));
+// })
+
+
+// console.log(val);

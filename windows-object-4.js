a64fe1734731a9300c23573396da4d921ebf24aa
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-09-22 11:46:11 +0100

    lesson 4

---
+++ windows-object-4.js
@@ -0,0 +1,39 @@
+// WINDOWS // OBJECTS // PROPERTIES
+
+// console.log("Hello world!");
+
+// Alert
+// alert("Hello world!");
+
+// PROMPT
+// const name = prompt('wagwan');
+// alert(name);
+
+// // CONFIRM
+// if(confirm('Are you sure you want to proceed?')) {
+//     console.log('Welcome to the page!');
+// }else {
+//     console.log('bye');
+// }
+
+
+// WINDOW INNERHEIGHT AND OUTERHEIGHT AND WIDTH
+// window.outerHeight;
+// window.innerHeight;
+// window.outerWidth;
+// window.innerWidth;
+
+// Scroll points
+
+// window.scrollX;
+// window.scrollY;
+
+//Location objects
+// window.location;
+// window.location.href;
+// redirecting
+// window.location.href = 'http://google.com';
+
+// History object
+// window.history.go(-1);
+// window.history.length;
